292c21291f908bc9b1c1cedb74f872f53bdf822d
Praveen7649 <dummy@gmail.com>
2025-12-04 12:46:34 +0000

    Day 3 Task

--- server/src/main/java/com/edutech/progressive/service/impl/CourseServiceImplJdbc.java
+++ server/src/main/java/com/edutech/progressive/service/impl/CourseServiceImplJdbc.java
@@ -4,6 +4,7 @@ import com.edutech.progressive.dao.CourseDAO;
 import com.edutech.progressive.entity.Course;
 import com.edutech.progressive.service.CourseService;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public class CourseServiceImplJdbc implements CourseService {
@@ -13,29 +14,49 @@ public class CourseServiceImplJdbc implements CourseService {
     public CourseServiceImplJdbc(CourseDAO courseDAO) {
         this.courseDAO = courseDAO;
     }
-    
+
     @Override
-    public List<Course> getAllCourses() {
-        return List.of();
+    public List<Course> getAllCourses() throws Exception {
+        try {
+            return courseDAO.getAllCourses();
+        } catch (SQLException e) {
+            throw new Exception("Error fetching all courses", e);
+        }
     }
 
     @Override
-    public Course getCourseById(int courseId) {
-        return null;
+    public Course getCourseById(int courseId) throws Exception {
+        try {
+            return courseDAO.getCourseById(courseId);
+        } catch (Exception e) {
+            throw new Exception("Error fetching course with ID " + courseId, e);
+        }
     }
 
     @Override
-    public Integer addCourse(Course course) {
-        return -1;
+    public Integer addCourse(Course course) throws Exception {
+        try {
+            return courseDAO.addCourse(course);
+        } catch (SQLException e) {
+            throw new Exception("Error adding course: " + course.getCourseName(), e);
+        }
     }
 
     @Override
-    public void updateCourse(Course course) {
-
+    public void updateCourse(Course course) throws Exception {
+        try {
+            courseDAO.updateCourse(course);
+        } catch (SQLException e) {
+            throw new Exception("Error updating course with ID " + course.getCourseId(), e);
+        }
     }
 
     @Override
-    public void deleteCourse(int courseId) {
-
+    public void deleteCourse(int courseId) throws Exception {
+        try {
+            courseDAO.deleteCourse(courseId);
+        } catch (SQLException e) {
+            throw new Exception("Error deleting course with ID " + courseId, e);
+        }
     }
 }
--- server/src/main/java/com/edutech/progressive/service/impl/StudentServiceImplJdbc.java
+++ server/src/main/java/com/edutech/progressive/service/impl/StudentServiceImplJdbc.java
@@ -4,6 +4,8 @@ import com.edutech.progressive.dao.StudentDAO;
 import com.edutech.progressive.entity.Student;
 import com.edutech.progressive.service.StudentService;
 
+import java.sql.SQLException;
+import java.util.Comparator;
 import java.util.List;
 
 public class StudentServiceImplJdbc implements StudentService {
@@ -13,32 +15,62 @@ public class StudentServiceImplJdbc implements StudentService {
     public StudentServiceImplJdbc(StudentDAO studentDAO) {
         this.studentDAO = studentDAO;
     }
-    
+
     @Override
-    public List<Student> getAllStudents() {
-        return List.of();
+    public List<Student> getAllStudents() throws Exception {
+        try {
+            return studentDAO.getAllStudents();
+        } catch (SQLException e) {
+            throw new Exception("Error fetching all students", e);
+        }
     }
 
     @Override
-    public Integer addStudent(Student student) {
-        return -1;
+    public Integer addStudent(Student student) throws Exception {
+        try {
+            return studentDAO.addStudent(student);
+        } catch (SQLException e) {
+            throw new Exception("Error adding student: " + student.getFullName(), e);
+        }
     }
 
     @Override
-    public List<Student> getAllStudentSortedByName() {
-        return List.of();
+    public List<Student> getAllStudentSortedByName() throws Exception {
+        try {
+            List<Student> sortedStudents = studentDAO.getAllStudents();
+            if (!sortedStudents.isEmpty()) {
+                sortedStudents.sort(Comparator.comparing(Student::getFullName));
+            }
+            return sortedStudents;
+        } catch (SQLException e) {
+            throw new Exception("Error fetching students sorted by Name ", e);
+        }
     }
 
     @Override
-    public void updateStudent(Student student) {
+    public void updateStudent(Student student) throws Exception {
+        try {
+            studentDAO.updateStudent(student);
+        } catch (SQLException e) {
+            throw new Exception("Error updating student with ID " + student.getStudentId(), e);
+        }
     }
 
     @Override
-    public void deleteStudent(int studentId) {
+    public void deleteStudent(int studentId) throws Exception {
+        try {
+            studentDAO.deleteStudent(studentId);
+        } catch (SQLException e) {
+            throw new Exception("Error deleting student with ID " + studentId, e);
+        }
     }
 
     @Override
-    public Student getStudentById(int studentId) {
-        return null;
+    public Student getStudentById(int studentId) throws Exception {
+        try {
+            return studentDAO.getStudentById(studentId);
+        } catch (Exception e) {
+            throw new Exception("Error fetching student with ID " + studentId, e);
+        }
     }
 }
