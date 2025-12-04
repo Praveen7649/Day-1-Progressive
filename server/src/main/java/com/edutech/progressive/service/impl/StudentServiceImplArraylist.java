8c1a529a5669251174dedcaa9893e4a7aede4d3f
Praveen7649 <dummy@gmail.com>
2025-12-04 13:32:15 +0000

    Day 5 Task

--- server/src/main/java/com/edutech/progressive/service/impl/StudentServiceImplArraylist.java
+++ server/src/main/java/com/edutech/progressive/service/impl/StudentServiceImplArraylist.java
@@ -2,11 +2,13 @@ package com.edutech.progressive.service.impl;
 
 import com.edutech.progressive.entity.Student;
 import com.edutech.progressive.service.StudentService;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
+@Service
 public class StudentServiceImplArraylist implements StudentService {
 
     private static List<Student> studentList = new ArrayList<>();
--- server/src/main/java/com/edutech/progressive/service/impl/StudentServiceImplJpa.java
+++ server/src/main/java/com/edutech/progressive/service/impl/StudentServiceImplJpa.java
@@ -1,5 +1,44 @@
 package com.edutech.progressive.service.impl;
 
-public class StudentServiceImplJpa  {
+import com.edutech.progressive.dto.StudentDTO;
+import com.edutech.progressive.entity.Student;
+import com.edutech.progressive.service.StudentService;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
+public class StudentServiceImplJpa implements StudentService {
+
+    @Override
+    public List<Student> getAllStudents() throws Exception {
+        return List.of();
+    }
+
+    @Override
+    public Integer addStudent(Student student) throws Exception {
+        return -1;
+    }
+
+    @Override
+    public List<Student> getAllStudentSortedByName() throws Exception {
+        return List.of();
+    }
+
+    @Override
+    public void updateStudent(Student student) throws Exception {
+    }
+
+    @Override
+    public void deleteStudent(int studentId) throws Exception {
+    }
+
+    @Override
+    public Student getStudentById(int studentId) throws Exception {
+        return null;
+    }
+
+    @Override
+    public void modifyStudentDetails(StudentDTO studentDTO) {
+    }
 }
